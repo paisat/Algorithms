4f4f502e3ca861e0b9b874f23129cfcf9de3332f
sarvotham pai <paisat007@gmail.com>
2016-01-18 14:56:34 -0800

    Url Shortener

--- src/main/java/hash/URLShotener.java
+++ src/main/java/hash/URLShotener.java
@@ -6,7 +6,55 @@ package hash;
 public class URLShotener {
 
 
-//    private
+    public static void main(String[] args) {
+
+        URLShotener obj = new URLShotener();
+        String shortenedUrl=obj.getShortenedUrl(12345);
+        System.out.println(shortenedUrl);
+        System.out.println(obj.getId(shortenedUrl));
+    }
+
+
+    public String getShortenedUrl(Integer id) {
+
+
+        String s = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+
+        char map[] = s.toCharArray();
+
+        StringBuilder shortUrl = new StringBuilder();
+
+        while (id != 0) {
+
+            shortUrl.insert(0, map[id % 62]);
+
+            id = id / 62;
+        }
+
+
+        return shortUrl.toString();
+
+    }
+
+
+    public Integer getId(String shortenedUrl) {
+
+        Integer id = 0; // initialize result
+
+        // A simple base conversion logic
+        for (int i = 0; i < shortenedUrl.length(); i++) {
+            char ch = shortenedUrl.charAt(i);
+
+            if ('a' <= ch && ch <= 'z')
+                id = id * 62 + ch - 'a';
+            if ('A' <= ch && ch <= 'Z')
+                id = id * 62 + ch - 'A' + 26;
+            if ('0' <= ch && ch <= '9')
+                id = id * 62 + ch - '0' + 52;
+        }
+        return id;
+
+    }
 
 
 }
